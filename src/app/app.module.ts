41161243220694bbb30ecd30184c8a8335adc19e
mayconxhh <mayconxhh@gmail.com>
2020-02-14 22:34:03 -0500

    se agrego servicios, rutas, interceptor, y componentes, funcion de login, registro y categorias (listar, agregar y ver detalles) completado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,90 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatMenuModule,
+  MatToolbarModule } from '@angular/material';
+
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { MatContenteditableModule } from 'mat-contenteditable';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { BycategoryComponent } from './bycategory/bycategory.component';
+import { DetailsComponent } from './details/details.component';
+import { CategoryComponent } from './category/category.component';
+import { CategoryDetailsComponent } from './category/category-details/category-details.component';
+import { CategoryAddComponent } from './category/category-add/category-add.component';
+import { CategoryEditComponent } from './category/category-edit/category-edit.component';
+import { PostComponent } from './post/post.component';
+import { PostDetailsComponent } from './post/post-details/post-details.component';
+import { PostAddComponent } from './post/post-add/post-add.component';
+import { PostEditComponent } from './post/post-edit/post-edit.component';
+
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenInterceptor } from './interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    AdminComponent,
+    BycategoryComponent,
+    DetailsComponent,
+    CategoryComponent,
+    CategoryDetailsComponent,
+    CategoryAddComponent,
+    CategoryEditComponent,
+    PostComponent,
+    PostDetailsComponent,
+    PostAddComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CKEditorModule,
+    MatContenteditableModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/category/category-add/category-add.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { CategoryService } from '../../category.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-category-add',
+  templateUrl: './category-add.component.html',
+  styleUrls: ['./category-add.component.scss']
+})
+export class CategoryAddComponent implements OnInit {
+
+  public Editor = ClassicEditor;
+
+  categoryForm: FormGroup;
+  catName = '';
+  catDesc = '';
+  catImgUrl = '';
+  catContent = '';
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(private router: Router, private api: CategoryService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.categoryForm = this.formBuilder.group({
+      catName : [null, Validators.required],
+      catDesc : [null, Validators.required],
+      catImgUrl : [null, Validators.required],
+      catContent : [null, Validators.required]
+    });
+  }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;
+    this.api.addCategory(this.categoryForm.value)
+      .subscribe((res: any) => {
+          const id = res._id;
+          this.isLoadingResults = false;
+          this.router.navigate(['category/details/', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+  }
+
+}
